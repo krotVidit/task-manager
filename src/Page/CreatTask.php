abce8859387fd59c142d1d4ce3d85cbdb2118162
KrotVidit <backendkid@mail.ru>
2023-05-29 23:06:27 +0300

    Добавлены элементы для выбора даты начала и оконачания задачи

--- src/Page/CreatTask.php
+++ src/Page/CreatTask.php
@@ -11,15 +11,29 @@ require_once("../block/header.php");
 <div class="mb-3 mt-4 text-center">
     <label for="exampleFormControlTextarea1" class="form-label">Описание Задачи</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-
 </div>
 
+<div class="container text-center">
+    <div class="row">
+
+            <div class="mb-3 col">
+                <label for="datepicker" class="form-label">Дата начала</label>
+                <input type="text" class="form-control" id="datepickerStart" name="dateStart" placeholder="Выберите дату">
+            </div>
+
+            <div class="mb-3 col">
+                <label for="datepicker" class="form-label">Дата окончание </label>
+                <input type="text" class="form-control" id="datepickerEnd" name="dateEnd" placeholder="Выберите дату">
+            </div>
+
+        </div>
+    </div>
 
 
 
 
 
 
-<?php
-require_once("../block/footer.php");
-?>
+    <?php
+    require_once("../block/footer.php");
+    ?>
--- src/block/header.php
+++ src/block/header.php
@@ -8,7 +8,11 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="./src/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+
 </head>
 
 <body>
@@ -30,8 +34,8 @@
                             </li>
                         </ul>
                         <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                            <input class="form-control me-2" type="search" placeholder="Введите имя задачи" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Поиск</button>
                         </form>
                     </div>
                 </div>
