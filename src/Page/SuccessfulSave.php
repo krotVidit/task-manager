692d6cdfb302f9e8a148cb4daf1bba5e71b99b60
KrotVidit <backendkid@mail.ru>
2023-05-30 13:52:06 +0300

    немного стилизованы кнопки главной страницы

--- src/Page/SuccessfulSave.php
+++ src/Page/SuccessfulSave.php
@@ -5,9 +5,8 @@ require_once("../block/header.php");
 <div class="text-center mt-5">
     <h1>Запись успешно сохранена </h1>
     <div class="row mt-5">
-        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover col " href="../Page/CreatTask.php">Создать новую задачу</a>
-
-        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover col" href="../index.php">Просмотреть созданные задачи </a>
+        <a class="btn btn-primary  col " href="../Page/CreatTask.php">Создать новую задачу</a>
+        <a class="btn btn-primary  col" href="../index.php">Просмотреть созданные задачи </a>
     </div>
 
 </div>
--- src/TaskInference.php
+++ src/TaskInference.php
@@ -1,10 +1,12 @@
-<div class="task-card bg-info text-white p-3 mb-4 rounded">
-    <h3 class="task-title"><?php echo $title; ?></h3>
-    <p class="task-description"><?php echo $description; ?></p>
-    <p class="task-dates">Дата начала: <?php echo $dateStart; ?>, Дата окончания: <?php echo $dateEnd; ?></p>
-    <div class="task-actions">
-        <a href="/edit.php?id=<?php echo $taskId; ?>" class="btn btn-light mr-2">Редактировать</a>
-        <span> </span>
-        <a href="/delete.php?id=<?php echo $taskId; ?>" class="btn btn-light">Удалить</a>
+<div class="card mt-3">
+    <div class="card-body">
+        <h3 class="task-title"><?php echo $title; ?></h3>
+        <p class="task-description"><?php echo $description; ?></p>
+        <p class="task-dates">Дата начала: <?php echo $dateStart; ?>, Дата окончания: <?php echo $dateEnd; ?></p>
+        <div class="task-actions">
+            <a href="/edit.php?id=<?php echo $taskId; ?>" class="btn btn-primary mr-2">Редактировать</a>
+            <span> </span>
+            <a href="/delete.php?id=<?php echo $taskId; ?>" class="btn btn-primary ">Удалить</a>
+        </div>
     </div>
 </div>
