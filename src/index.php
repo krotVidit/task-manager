9103932b4b4745accadaa65f1af5291537e94c1f
KrotVidit <backendkid@mail.ru>
2023-05-30 13:41:36 +0300

    Главная страница ,Html код выведен в отдельный фай

--- src/index.php
+++ src/index.php
@@ -17,16 +17,7 @@ if ($result->num_rows > 0) {
         $dateStart = $row['dateStart'];
         $dateEnd = $row['dateEnd'];
 
-        echo '<div class="task-card bg-info text-white p-3 mb-4 rounded">';
-        echo '<h3 class="task-title">' . $title . '</h3>';
-        echo '<p class="task-description">' . $description . '</p>';
-        echo '<p class="task-dates">Дата начала: ' . $dateStart . ', Дата окончания: ' . $dateEnd . '</p>';
-        echo '<div class="task-actions">';
-        echo '<a href="/edit.php?id=' . $taskId . '" class="btn btn-light mr-2">Редактировать</a>';
-        echo "<span>   </span>";
-        echo '<a href="/delete.php?id=' . $taskId . '" class="btn btn-light">Удалить</a>';
-        echo '</div>';
-        echo '</div>';
+      require("../src/TaskInference.php");
     }
 } else {
     echo '<p>Нет созданных задач.</p>';
