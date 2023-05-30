17ab1fa9753c69c4f8a33a242fbb5866a044e2a6
KrotVidit <backendkid@mail.ru>
2023-05-30 04:40:52 +0300

    Страница успешного вывода создание задачи

--- src/Handlers/HandlerCreatTask.php
+++ src/Handlers/HandlerCreatTask.php
@@ -4,12 +4,15 @@ require_once("../connect.php");
 $title = $_POST['title'];
 $description = $_POST['description'];
 $dateStart = $_POST['dateStart'];
-$dateEnd = isset($_POST['dateEnd']) ? $_POST['dateEnd'] : null;
+$dateEnd = $_POST['dateEnd']; 
+
 
 
 $sql = "INSERT INTO $tabl (title, description, dateStart, dateEnd) VALUES ('$title', '$description', '$dateStart', '$dateEnd')";
 if ($mysql->query($sql)) {
-    echo "Данные успешно сохранены.";
+    require_once("../Page/SuccessfulSave.php");
+  
+ 
     
 } else {
     echo "Ошибка при сохранении данных: " . $mysql->error;
