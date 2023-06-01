ea9c67bf1c5936e47ad8746e64629cbfb94cda00
KrotVidit <backendkid@mail.ru>
2023-06-01 14:25:25 +0300

    Убраны Row и col для более коректного отображения

--- src/Page/NoTasskCreated.php
+++ src/Page/NoTasskCreated.php
@@ -1,8 +1,8 @@
 
 <div class="text-center mt-5">
     <h1>Нет созданных задач</h1>
-    <div class="row mt-5">
-        <a class="btn btn-primary  col " href="/src/Page/CreatTask.php">Создать новую задачу</a>
+    <div class="mt-5">
+        <a class="btn btn-primary " href="/src/Page/CreatTask.php">Создать новую задачу</a>
     </div>
 
 </div>
--- src/Page/SuccessfulSave.php
+++ src/Page/SuccessfulSave.php
@@ -5,9 +5,9 @@ require_once("../block/header.php");
 
 <div class="text-center mt-5">
     <h1>Задача успешно сохранена </h1>
-    <div class="row mt-5">
-        <a class="btn btn-primary  col " href="../Page/CreatTask.php">Создать новую задачу</a>
-        <a class="btn btn-primary  col" href="../index.php">Просмотреть созданные задачи </a>
+    <div class=" mt-5">
+        <a class="btn btn-primary " href="../Page/CreatTask.php">Создать новую задачу</a>
+        <a class="btn btn-primary  " href="../index.php">Просмотреть созданные задачи </a>
     </div>
 
 </div>
